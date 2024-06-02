d278af5f94c6c7b80caf1b48f9f1085005bd4585
git12nazneen <lipinazneen65@gmail.com>
2024-06-03 02:08:29 +0600

    added detailse camp

--- index.js
+++ index.js
@@ -152,6 +152,13 @@ app.use(
             res.send(result);
         })
 
+      app.get('/camps/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query= {_id : new ObjectId(id)}
+      const result = await addCampCollection.findOne(query)
+      res.send(result)
+    })
+
     //   await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
